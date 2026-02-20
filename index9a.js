1090f64621bd9380801aa4602613325db8ba68db
suryadwd <surajdwivedi644@gmail.com>
2026-02-20 15:52:08 +0530

    added

--- index9a.js
+++ index9a.js
@@ -6,11 +6,11 @@ const auth = (req, res, next) => {
     if(req.url == pass) next();
     else res.send("unauthorized")
 }
+
 app.use(auth)
-app.get( "/:x",  (req,  res) => {
-    console.log(req.params.x)
-    
-    res.send("wlc")
-} )
+
+app.get( "/:x",  (req,  res) => { res.send("wlc") })
+
+
 
 app.listen(8888, () => {console.log("server is live")})
