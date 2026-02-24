70f81c28eaae40b717a1ff8f0ff66368982707ca
suryadwd <surajdwivedi644@gmail.com>
2026-02-24 09:20:15 +0530

    update

--- index2.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-const app = express()
-app.listen(8080,()=>{
-    console.log("server is runnig")
-})
-
-app.get("/", (req, res)=> {
-    return  res.send("get api")
-})
